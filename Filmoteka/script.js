5134300e98dba491416e7250ad41d38bc0d9918e
MarznaHuzei <marinka777xx@gmail.com>
2023-11-16 22:27:16 +0100

    film

---
+++ Filmoteka/script.js
@@ -0,0 +1,155 @@
+// Створи фільмотеку з популярними фільмами, для цього використай
+// https://developer.themoviedb.org/reference/trending-movies
+
+// Щоб отримати постер фільму потрібно підставити url з відповіді від бекенду та url з документації
+// https://developer.themoviedb.org/docs/image-basics
+
+// Відмалюй картки з фільмами
+// Приклад картки  => https://prnt.sc/Hi_iLLg7Nd1F
+
+// Реалізуй пагінацію
+// 1 Кнопка "Load More"
+// 2 Infinity scroll (https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API)
+
+// const elements = {
+//   list: document.querySelector(".js-movie-list"),
+//   btnLoad: document.querySelector(".js-load-more"),
+// };
+
+// let page = 1;
+
+// elements.btnLoad.addEventListener("click", handlerLoadMore);
+
+// function handlerLoadMore() {
+//   page += 1;
+//   serviceMovie(page).then((data) => {
+//     elements.list.insertAdjacentHTML("beforeend", createMarkup(data.results));
+//     if (data.page >= 500 || data.page >= data.total_pages) {
+//       elements.btnLoad.classList.replace("load-more", "load-more-hidden");
+//     }
+//   });
+// }
+
+// function serviceMovie(page = 1) {
+//   const BASE_URL = "https://api.themoviedb.org/3/";
+//   const END_POINT = "trending/movie/week";
+//   const API_KEY = "345007f9ab440e5b86cef51be6397df1";
+
+//   const params = new URLSearchParams({
+//     api_key: API_KEY,
+//     page,
+//   });
+//   console.log(params.toString());
+//   return fetch(`${BASE_URL}${END_POINT}?${params}`).then((resp) => {
+//     if (!resp.ok) {
+//       throw new Error(resp.statusText || "Error");
+//     }
+//     return resp.json();
+//   });
+// }
+
+// serviceMovie()
+//   .then((data) => {
+//     elements.list.insertAdjacentHTML("afterbegin", createMarkup(data.results));
+
+//     if (data.page < data.total_pages) {
+//       elements.btnLoad.classList.replace("load-more-hidden", "load-more");
+//     }
+//   })
+//   .catch((err) => console.log(err));
+
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({ poster_path, original_title, release_date, vote_average }) =>
+//         `  <li class= "movie-card">
+//         <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${original_title}" />
+//         <h2>${original_title}</h2>
+//        <div class="movie-info">
+//         <p>Release date :${release_date}</p>
+//         <p>Vote average :${vote_average}</p>
+//        </div>
+//       </li>
+//   `
+//     )
+//     .join("");
+// }
+
+const elements = {
+  list: document.querySelector(".js-movie-list"),
+  guard: document.querySelector(".js-guard"),
+};
+
+const options = {
+  root: null,
+  rootMargin: "300px",
+  threshold: 1.0,
+};
+
+const observer = new IntersectionObserver(handlerLoadMore, options);
+
+let page = 498;
+
+function serviceMovie(page = 1) {
+  const BASE_URL = "https://api.themoviedb.org/3/";
+  const END_POINT = "trending/movie/week";
+  const API_KEY = "345007f9ab440e5b86cef51be6397df1";
+
+  const params = new URLSearchParams({
+    api_key: API_KEY,
+    page,
+  });
+  console.log(params.toString());
+  return fetch(`${BASE_URL}${END_POINT}?${params}`).then((resp) => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText || "Error");
+    }
+    return resp.json();
+  });
+}
+
+serviceMovie()
+  .then((data) => {
+    elements.list.insertAdjacentHTML("afterbegin", createMarkup(data.results));
+
+    if (data.page < data.total_pages) {
+      observer.observe(elements.guard);
+    }
+  })
+  .catch((err) => console.log(err));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ poster_path, original_title, release_date, vote_average }) =>
+        `  <li class= "movie-card">
+        <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${original_title}" />
+        <h2>${original_title}</h2>
+       <div class="movie-info">
+        <p>Release date :${release_date}</p>
+        <p>Vote average :${vote_average}</p>
+       </div>
+      </li>
+  `
+    )
+    .join("");
+}
+
+function handlerLoadMore(entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      page += 1;
+      serviceMovie(page)
+        .then((data) => {
+          elements.list.insertAdjacentHTML(
+            "beforeend",
+            createMarkup(data.results)
+          );
+          if (data.page >= data.total_pages || data.page >= 500) {
+            observer.unobserve(elements.guard);
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  });
+}
